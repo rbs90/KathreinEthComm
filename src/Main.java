10954c1cc62de6d98cdb692b72f0c2140af177f6
Robert Schönherr <rbs@informatik.tu-chemnitz.de>
2012-04-24 17:36:18 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rbs
+ * Date: 02.04.12
+ * Time: 14:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new EthConnector();
+    }
+}
